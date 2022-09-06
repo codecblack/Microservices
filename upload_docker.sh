fee2c3a326447986cfca5cf0ecdf9ea9568e1744
EC2 Default User <ec2-user@ip-172-31-84-40.ec2.internal>
2022-09-06 01:10:47 +0000

    Added files

---
+++ upload_docker.sh
@@ -0,0 +1,18 @@
+!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath=codecblack/udacity:v1
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker login -u codecblack -p ****** docker.io
+docker tag $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
